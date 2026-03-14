82de9018ad99214886c8f9e46cfabbefd43d2d24
Brenda2310 <brendafernandez2310@gmail.com>
2026-03-14 19:41:41 -0300

    Product Controller

--- product-service/src/main/java/com/electrotienda/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/electrotienda/product_service/controller/ProductController.java
@@ -1,13 +1,33 @@
 package com.electrotienda.product_service.controller;
 
+import com.electrotienda.product_service.dto.ProductDTO;
+import com.electrotienda.product_service.entity.ProductEntity;
 import com.electrotienda.product_service.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-public class ProductController {
+public class ProductController{
     private final ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<ProductDTO>> getAllProducts(){
+        return ResponseEntity.ok(service.getAllProducts());
+    }
+
+    @GetMapping
+    public ResponseEntity<ProductDTO> getProductById(@PathVariable Long id){
+        return ResponseEntity.ok(service.getProductById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO product){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createProduct(product));
+    }
 }
