a3e065a0e5b4f57a9035be5c488823924c7769a9
Brenda2310 <brendafernandez2310@gmail.com>
2026-03-17 16:45:59 -0300

    Notification server change

--- notification-service/src/main/java/com/electrotienda/notification_service/consumer/NotificationConsumer.java
+++ notification-service/src/main/java/com/electrotienda/notification_service/consumer/NotificationConsumer.java
@@ -4,6 +4,7 @@ import com.electrotienda.notification_service.dto.SaleCreatedEvent;
 import com.electrotienda.notification_service.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class NotificationConsumer {
     private final EmailService emailService;
 
+    @KafkaListener(topics = "notification-topic", groupId = "notification-group")
     public void consumeSaleEvent(SaleCreatedEvent event){
         log.info("Evento recibido desde Kafka. Procesando notificación para Venta ID: {}", event.getSaleId());
 
